47a14bcc1d0cceceadd56a8bf7bb6f5c87e74d27
Luke <lukembeck@gmail.com>
2019-03-01 00:26:00 +1000

    Updates

---
+++ precache-manifest.e9dbc704be4958ddb6d97e3857410522.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ba4d36ed55dc40e3afc",
+    "url": "/jigen/static/js/main.9ba4d36e.chunk.js"
+  },
+  {
+    "revision": "f385ea00a126c673e678",
+    "url": "/jigen/static/js/runtime~main.f385ea00.js"
+  },
+  {
+    "revision": "af5fb8642af33c1083ff",
+    "url": "/jigen/static/css/2.595b5ead.chunk.css"
+  },
+  {
+    "revision": "af5fb8642af33c1083ff",
+    "url": "/jigen/static/js/2.af5fb864.chunk.js"
+  },
+  {
+    "revision": "e86d53af6697d54d52442685d80b0fb5",
+    "url": "/jigen/static/media/logo.e86d53af.svg"
+  },
+  {
+    "revision": "80061900b83dcfeaf471cfc6a48759d9",
+    "url": "/jigen/index.html"
+  }
+];
